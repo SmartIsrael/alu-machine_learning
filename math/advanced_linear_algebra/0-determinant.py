0d03bf30ecbd68e25e163e4491dc130dca7dcfbf
Smart Israel <116894867+SmartIsrael@users.noreply.github.com>
2024-05-23 23:17:49 +0200

    done with assignment

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
+"""Getting the determinant of a matrix"""
+
+
 def determinant(matrix):
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    """Function that calculates the determinant of a matrix """
+    if not all(type(row) == list for row in matrix):
         raise TypeError("matrix must be a list of lists")
-
-    if len(matrix) == 0:  # Handle empty matrix case
+    if len(matrix) == 0 or type(matrix) != list:
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
         return 1
-
-    if len(matrix) != len(matrix[0]):
+    if not all(len(r) == len(matrix) for r in matrix):
         raise ValueError("matrix must be a square matrix")
-
-    n = len(matrix)
-
-    if n == 1:
+    if len(matrix) == 1:
         return matrix[0][0]
-    elif n == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    else:
-        det = 0
-        for i in range(n):
-            submatrix = [row[:i] + row[i+1:] for row in (matrix[:j] + matrix[j+1:]
-                         for j in range(1, n))]
-            det += ((-1)**i) * matrix[0][i] * determinant(submatrix)
-        return det
+    if len(matrix) == 2:
+        x = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+        return x
+    det = 0
+    for x, num in enumerate(matrix):
+        temp = []
+        P = matrix[0][x]
+        for row in matrix[1:]:
+            new = []
+            for j in range(len(matrix)):
+                if j != x:
+                    new.append(row[j])
+            temp.append(new)
+        det += P * determinant(temp) * (-1) ** x
+    return det
